2feb77212441a6ed3ea88fc14282f273184c76c8
Joey Balardeta <jgbalardeta@gmail.com>
2022-02-26 15:43:30 -0800

    Start and end screen added, collision checking added.

--- GeoHelixData.py
+++ GeoHelixData.py
@@ -1,3 +1,10 @@
+from pygame import Rect
+import random
+
+WIDTH = 400
+HEIGHT = 600
+RECT_MOVE_SPEED = 2
+
 class Rectangle:
     Rects = []
 
@@ -6,6 +13,7 @@ class Rectangle:
         self.y = y
         self.w = w
         self.h = h
+        self.color = (random.randint(150, 255), random.randint(150, 255), random.randint(150, 255))
         Rectangle.Rects.append(self)
 
     def move(self, x, y):
@@ -16,9 +24,94 @@ class Rectangle:
         self.x += x
         self.y += y
 
+
     def destroy(self):
         '''
         remove rectangle from list of all rectangles
         '''
         
         Rectangle.Rects.remove(self)
+
+    def rectCheck():
+        for rect in Rectangle.Rects:
+            if (rect.y > HEIGHT):
+                rect.destroy()
+
+class Player:
+    def __init__(self):
+        self.w = 20
+        self.h = 20
+        self.x = 10
+        self.y = 550
+        self.health = 1
+
+
+    def swap_sides(self):
+        if (self.x == 10):
+            self.x = WIDTH - 30
+            return
+        
+        if (self.x == WIDTH - 30):
+            self.x = 10
+            return
+
+    def collision_check(self):
+        for rect in Rectangle.Rects:
+            if ((self.x in range(rect.x, rect.x + rect.w)) or ((self.x + self.w) in range(rect.x, rect.x + rect.w))):
+                if ((self.y in range(rect.y, rect.y + rect.h)) or ((self.y + self.h) in range(rect.y, rect.y + rect.h))):
+                    self.health -= 1
+
+
+class Obstacle:
+    '''
+    Form horizontal stacks of rectangles to create wall obstacles
+    '''
+    def __init__(self, height, side):
+        self.height = height
+        self.components = []
+        for r in range(height):
+            if (side % 2 == 0):
+                rect = Rectangle((10 + (r * 20)), (-40), 20, 20)
+                self.components.append(rect)
+            
+            else:
+                rect = Rectangle((WIDTH - 30 - (r * 20)), (-40), 20, 20)
+                self.components.append(rect)
+
+
+class PhysicsEngine:
+    '''
+    Costantly move all rectangles (that are part of obstacle objects) downward
+    '''
+    def __init__(self, player):
+        self.player = player
+    
+    def simulate(self):
+        for rect in Rectangle.Rects:
+            rect.move(0, RECT_MOVE_SPEED)
+        
+        Rectangle.rectCheck()
+        self.player.collision_check()
+
+
+
+class ObstacleGenerator():
+    '''
+    Randomly generate an Obstacle of random length based on game time
+    '''
+    def __init__(self):
+        self.time_since_last_obstacle = 0
+        self.last_obstacle_side = 1
+    
+    def obstacleConsider(self, game_time):
+        self.time_since_last_obstacle += RECT_MOVE_SPEED
+        if (game_time % 20 == 0):
+            spawnChance = random.randint(100, 500)
+            if (spawnChance < self.time_since_last_obstacle):
+                self.spawnObstacle()
+
+
+    def spawnObstacle(self):
+        Obstacle(random.randint(2, 10), self.last_obstacle_side)
+        self.time_since_last_obstacle = 0
+        self.last_obstacle_side = (self.last_obstacle_side + 1) % 2
--- GeoHelix_Engine.py
+++ GeoHelix_Engine.py
@@ -1,4 +1,3 @@
-from sre_parse import WHITESPACE
 import GeoHelixData
 import time, pygame
 
@@ -6,72 +5,140 @@ import time, pygame
 # Global Constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+BLUE = (3, 157, 252)
+RED = (255, 54, 54)
 
 class GeoHelix:
     def __init__(self):
-        self.WIDTH = 400
-        self.HEIGHT = 600
+        self.WIDTH = GeoHelixData.WIDTH
+        self.HEIGHT = GeoHelixData.HEIGHT
         self.time_elapsed = 0
+        self.delayHz = 120
         self.running = True
+        pygame.init()
+        pygame.font.init()
         self.renderer = Renderer(self)
+        self.player = GeoHelixData.Player()
+        self.physicsEngine = GeoHelixData.PhysicsEngine(self.player)
+        self.obstacleGenerator = GeoHelixData.ObstacleGenerator()
+        
 
         # Don't put anything after this, it won't run until the end of the program
         self.start()
-        
-        
+              
 
     def start(self):
-        pygame.init()
         try:
             self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+            pygame.display.set_caption("GeoHelix - HackUCI 2022")
             self.game_loop()
         
         finally:
             pygame.quit()
     
     def game_loop(self):
+        self.start_screen()
         while self.running:
-            self.events()
+            self.handle_events()
             self.renderer.render()
+            self.physicsEngine.simulate()
+            self.obstacleGenerator.obstacleConsider(self.time_elapsed)
+            self.time_elapsed += 1
+            self.difficulty_tweak()
+            # print(len(GeoHelixData.Rectangle.Rects))
+            time.sleep(1.0/self.delayHz)
+        
+        self.game_end()
             
-    def events(self):
+    def handle_events(self):
+        if (self.player.health < 1):
+            self.game_stop()
+            time.sleep(0.5)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self._end_game()
+                self.game_stop()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.swap_sides()
 
-    def _end_game(self):
+
+    def difficulty_tweak(self):
+        newDiff = int(self.time_elapsed / 1000)
+        if (newDiff > 2):
+            GeoHelixData.RECT_MOVE_SPEED = newDiff
+
+    def start_screen(self):
+        self.screen.fill(BLACK)
+        welcome = self.renderer.font.render("Welcome to GeoHelix!", False, BLUE)
+        welcome2 = self.renderer.font.render("Don't touch the boxes!", False, RED)
+        welcome3 = self.renderer.font.render("Press SPACE to switch sides!", False, WHITE)
+        welcome_rect = welcome.get_rect(center = (GeoHelixData.WIDTH / 2, (GeoHelixData.HEIGHT / 2) - 25))
+        welcome2_rect = welcome2.get_rect(center = (GeoHelixData.WIDTH / 2, GeoHelixData.HEIGHT / 2))
+        welcome3_rect = welcome3.get_rect(center = (GeoHelixData.WIDTH / 2, (GeoHelixData.HEIGHT / 2) + 25))
+        self.screen.blit(welcome, welcome_rect)
+        self.screen.blit(welcome2, welcome2_rect)
+        self.screen.blit(welcome3, welcome3_rect)
+        pygame.display.flip()
+        time.sleep(5)
+
+    def game_stop(self):
         self.running = False
+        
 
+    def game_end(self):
+        self.screen.fill(BLACK)
+        gameOver = self.renderer.font.render("Game Over!", False, RED)
+        score = self.renderer.font.render("Final Score: " + str(int(self.time_elapsed / 100)), False, WHITE)
+        gameOver_rect = gameOver.get_rect(center = (GeoHelixData.WIDTH / 2, (GeoHelixData.HEIGHT / 2) - 25))
+        score_rect = score.get_rect(center = (GeoHelixData.WIDTH / 2, GeoHelixData.HEIGHT / 2))
+        self.screen.blit(gameOver, gameOver_rect)
+        self.screen.blit(score, score_rect)
+        pygame.display.flip()
+        time.sleep(2)
 
 class Renderer:
     def __init__(self, GeoHelixObject):
         # Get copy of GeoHelix object in memory
         self.GeoHelixGame = GeoHelixObject
+        self.font = pygame.font.SysFont('Calibri', 18)
 
 
     # In charge of all display functions
     def render(self):
         # Draw all game objects
+        self.GeoHelixGame.screen.fill(BLACK)
         self.draw_sidelines()
-        self.draw_rect()
         self.draw_player()
+        for rect in GeoHelixData.Rectangle.Rects:
+            self.draw_rect(rect)
+        
+        self.draw_score()
 
         # Refresh display
         pygame.display.flip()
 
 
+    def draw_score(self):
+        score = self.font.render("Score: " + str(int(self.GeoHelixGame.time_elapsed / 100)), False, WHITE)
+        speed = self.font.render("Speed: " + str(GeoHelixData.RECT_MOVE_SPEED), False, WHITE)
+        self.GeoHelixGame.screen.blit(score, (15, 0))
+        self.GeoHelixGame.screen.blit(speed, (15, 20))
+
     # Draw sidelines with "fade-to-gray" gradient
     def draw_sidelines(self):
         for i in range(10):
-            pygame.draw.line(self.GeoHelixGame.screen, (255 - i * 20, 255 - i * 20, 255 - i * 20), (10 - i, 0),(10 - i, self.GeoHelixGame.HEIGHT))
-            pygame.draw.line(self.GeoHelixGame.screen, (255 - i * 20, 255 - i * 20, 255 - i * 20), (self.GeoHelixGame.WIDTH - 11 + i, 0),(self.GeoHelixGame.WIDTH - 11 + i, self.GeoHelixGame.HEIGHT))
+            pygame.draw.line(self.GeoHelixGame.screen, (255 - i * 20, 255 - i * 20, 255 - i * 20), (9 - i, 0),(9 - i, self.GeoHelixGame.HEIGHT))
+            pygame.draw.line(self.GeoHelixGame.screen, (255 - i * 20, 255 - i * 20, 255 - i * 20), (self.GeoHelixGame.WIDTH - 10 + i, 0),(self.GeoHelixGame.WIDTH - 10 + i, self.GeoHelixGame.HEIGHT))
 
-    def draw_rect(self):
-        pass
+    def draw_rect(self, rect):
+        rectangle = pygame.Rect(rect.x, rect.y, rect.w, rect.h)
+        pygame.draw.rect(self.GeoHelixGame.screen, rect.color, rectangle, 1)
 
 
     def draw_player(self):
-        pass
+        for i in range(int(self.GeoHelixGame.player.w / 4)):
+            rectangle = pygame.Rect(self.GeoHelixGame.player.x + (i * 2), self.GeoHelixGame.player.y + (i * 2), self.GeoHelixGame.player.w - (i * 4), self.GeoHelixGame.player.h - (i * 4))
+            pygame.draw.rect(self.GeoHelixGame.screen, WHITE, rectangle, 1)
 
 
 def main():
